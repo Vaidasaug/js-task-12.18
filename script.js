46ed10d72ac2ff95e37e606d164081841265e95b
Vaida Česevičienė <waidaxxx@Vaidas-MacBook-Pro.local>
2025-12-18 16:37:26 +0200

    uncomented

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 //---1-- Parašyti fn kuri grąžina stataus trikampio plotą;
 // Trikampiui yra paduodamas trikampio ilgis ir plotis;
 
-// const triangle = (length, width) => {
-//   console.log((length * width) / 2);
-// };
+const triangle = (length, width) => {
+  console.log((length * width) / 2);
+};
 
-// triangle(5, 4);
+triangle(5, 4);
 
 //---2---- Parašyt fn kuri apskaičiuoja apskritimo plotą;
 //  Į fn turi būt paduodamas skritulio spindulys(puse skersmens);
 
-// const circle = (r) => {
-//   console.log(r ** 2 * 3.14);
-// };
+const circle = (r) => {
+  console.log(r ** 2 * 3.14);
+};
 
-// circle(6);
+circle(6);
 
 //---3----Parašyti funkcija kuri atvaizduoja atvirkščia piramidę sukurtą iš #.
 // PVZ.: paduodamas skaičius 4
@@ -23,13 +23,13 @@
 // ##
 // #
 
-// const hashTagRepeat = (hash) => {
-//   for (let i = hash; i > 0; i--) {
-//     console.log("#".repeat(i));
-//   }
-// };
+const hashTagRepeat = (hash) => {
+  for (let i = hash; i > 0; i--) {
+    console.log("#".repeat(i));
+  }
+};
 
-// hashTagRepeat(4);
+hashTagRepeat(4);
 
 //---4---Parašyti funkcija kuri atvaizduoja šukas iš  #.
 // PVZ.: paduodamas skaičius 3
@@ -39,49 +39,49 @@
 // #
 // ###
 
-// const getFigure = (num) => {
-//   for (let i = 0; i < num; i = i + 1) {
-//     if (i % 2 === 0) {
-//       console.log("#".repeat(num));
-//     } else {
-//       console.log("#");
-//     }
-//   }
-// };
-// getFigure(5);
+const getFigure = (num) => {
+  for (let i = 0; i < num; i = i + 1) {
+    if (i % 2 === 0) {
+      console.log("#".repeat(num));
+    } else {
+      console.log("#");
+    }
+  }
+};
+getFigure(6);
 
 //---5-- Sukurti fn kuri priima kaip argumentą 1sk.
 // sKaičius turi būt nuo 1 iki 6.
 // Jeigu paduodama 11 - funkcija turi returnint tekstą "Bad provided arg";
 //(cia kaip ir viskas ok GAL) kaip toliau 5.2?....)
 
-// const numberFragment = (num) => {
-//   if (num <= 6 && num >= 1) {
-//     return num;
-//   } else {
-//     return "Bad provided arg";
-//   }
-// };
-// const result = numberFragment(-1);
-// console.log(result);
+const numberFragment = (num) => {
+  if (num <= 6 && num >= 1) {
+    return num;
+  } else {
+    return "Bad provided arg";
+  }
+};
+const resultfinal = numberFragment(-1);
+console.log(resultfinal);
 
 //---5.2---- (chat gpt vel i pagalba... :( ....)
 
-// const numberGame = (num) => {
-//   if (num < 1 || num > 6) {
-//     return "Bad provided arg";
-//   }
+const numberGame = (num) => {
+  if (num < 1 || num > 6) {
+    return "Bad provided arg";
+  }
 
-//   const random = Math.floor(Math.random() * 6) + 1;
+  const random = Math.floor(Math.random() * 6) + 1;
 
-//   if (num === random) {
-//     return "Success " + num + " " + random;
-//   } else {
-//     return "You lost, try again " + num + " " + random;
-//   }
-// };
-// const result = numberGame(6);
-// console.log(result);
+  if (num === random) {
+    return "Success " + num + " " + random;
+  } else {
+    return "You lost, try again " + num + " " + random;
+  }
+};
+const result = numberGame(6);
+console.log(result);
 
 ///--6---- Parašyti funkcija kuri atvaizduoja kvadratą sukurtą iš #.
 // PVZ.: paduodamas skaičius 3
@@ -89,9 +89,9 @@
 // ###
 // ###
 
-// const getSquare = (num) => {
-//   for (let i = 0; i < num; i = i + 1) {
-//     console.log("#".repeat(num));
-//   }
-// };
-// getSquare(5);
+const getSquare = (num) => {
+  for (let i = 0; i < num; i = i + 1) {
+    console.log("#".repeat(num));
+  }
+};
+getSquare(5);
